b9b9568f4ea4ae747a85dd75155618708fdcc2ef
joajoanita <joanita300123@gmail.com>
2025-04-07 21:08:04 +0200

    Borrow without functionality

--- views/borrow/index.php
+++ views/borrow/index.php
@@ -1,14 +1,14 @@
-<div class="row d-flex">
+<div class="row d-flex justify-content-sm-center">
     <?php foreach ($viewmodel as $item): ?>
-        <div class="col-md-6 col-lg-6 col-xl-4 mb-4">
+        <div class="col-md-9 col-lg-6 col-xl-6 col-xxl-4 mb-4">
             <div class="shadow rounded-4 bg-color py-4 px-4">
                 <img src="<?php echo ROOT_URL.'assets/'.$item['image'];?>" alt="<?php echo $item['title']?>" class="img-fluid img-borrow rounded-2 object-fit-cover">
                 <h3 class="mt-3"><?php echo $item['title'];?></h3>
                 <h6><?php echo $item['author'];?></h6>
                 <p class="text-clamp"><?php echo $item['description'];?></p>
-                <div class="d-flex justify-content-center gap-4">
-                    <button class="btn btn-primary-color shadow w-50">Borrow</button>
-                    <button class="btn btn-primary-color shadow w-50">See More</button>
+                <div class="d-flex justify-content-center gap-4 flex-sm-row flex-column">
+                    <button class="btn btn-primary-color shadow w-50 w-sm-100">Borrow</button>
+                    <button class="btn btn-primary-color shadow w-50 w-sm-100">See More</button>
                 </div>
             </div>
         </div>
